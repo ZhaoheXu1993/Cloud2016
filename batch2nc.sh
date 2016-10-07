c85a6df19928bebc5528d6f197936a566a9cb52e
Joe Xu <zhaohexu1993@gmail.com>
2016-10-07 14:01:18 -0400

    add sudo in batch2nc.sh

--- batch2nc.sh
+++ batch2nc.sh
@@ -11,7 +11,7 @@ echo "Finish extract!"
 
 for x in `ls`; do 
     if [[ $x =~ ^K ]]; then 
-        java -classpath ~/Downloads/toolsUI-4.6.6.jar ucar.nc2.dataset.NetcdfDataset -in $x -out $x.nc 
+        sudo java -classpath ~/Downloads/toolsUI-4.6.6.jar ucar.nc2.dataset.NetcdfDataset -in $x -out $x.nc 
     fi; 
 done
 
