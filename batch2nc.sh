fdee58cb32d498f9083fc5c0523a674eec2acf16
miaoji <admin@hotel-location.net>
2016-10-22 18:17:26 -0400

    new netcdf jar

--- batch2nc.sh
+++ batch2nc.sh
@@ -8,8 +8,8 @@ done
 echo "Finish extract!"
 
 for x in `ls`; do 
-    if [[ $x =~ ^KTLH ]]; then 
-         /c/ProgramData/Oracle/Java/javapath/java.exe -classpath toolsUI-4.6.6.jar ucar.nc2.dataset.NetcdfDataset -in $x -out $x.nc 
+    if [[ $x =~ ^K ]]; then 
+         java -classpath netcdfAll-4.6.6.jar ucar.nc2.dataset.NetcdfDataset -in $x -out $x.nc 
     fi; 
 done
 
