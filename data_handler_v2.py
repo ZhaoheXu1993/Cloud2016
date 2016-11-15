fe3a5c56ea3f281a7780d732501a7c18f2735685
sugar <sugar1987cn@gmail.com>
2016-11-15 11:14:07 -0500

    Push

--- data_handler_v2.py
+++ data_handler_v2.py
@@ -8,6 +8,7 @@ from sortedcontainers import SortedDict as OrderedDict
 from functools import reduce
 
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 import json
@@ -17,17 +18,74 @@ from pprint import pprint as pp
 import numpy
 import numpy.ma
 
+import numexpr
+
 import netCDF4
 from netCDF4 import Dataset, num2date
 
 from typing import List, Dict, Tuple
 
 
-@jit
+@jit(nopython=True)
 def find_elevation_index(vcp_mode, e):
+    j = 0
     for i in vcp_mode:
         if -0.2 < e - i < 0.2:
-            return i
+            return j
+        j += 1
+
+@jit(nopython=True)
+def fill_data_jit(gate_array, azimuth_array, dump_array, elevation_index, size_y, size_z):
+    r = gate_array[:size_y, :]
+    a = azimuth_array[:size_y]
+    # Determine azimuthal resolution
+    ad = a[1:] - a[:-1]
+    # This is a slow way to determine if it is truly high resolution.
+    # Well, maybe not necessary as we patched netcdf jar.
+    # TODO: Remove this, only relies on "_HI" or not.
+    az_downscale = round(numpy.mean(ad[ad > 0]),1) > 0.7
+    # As long as it is not legacy, we expand it to 720
+    # Then we fill the shape
+    if az_downscale:
+        for k in range(r.shape[0]):
+            az_index = int(a[k] * 2)
+            dump_array[elevation_index, az_index % size_y, :] = r[k, :size_z]
+            dump_array[elevation_index, (az_index + 1) % size_y, :] = r[k, :size_z]
+    else:
+        for k in range(r.shape[0]):
+            az_index = int(a[k] * 2)
+            dump_array[elevation_index, az_index % size_y, :] = r[k, :size_z]
+    pass
+
+
+def fill_data_nojit(gate_array, azimuth_array, dump_array, elevation_index, size_y, size_z):
+    r = gate_array[:size_y, :]
+    a = azimuth_array[:size_y]
+    # Determine azimuthal resolution
+    ad = numpy.ediff1d(a)
+    # This is a slow way to determine if it is truly high resolution.
+    # Well, maybe not necessary as we patched netcdf jar.
+    # TODO: Remove this, only relies on "_HI" or not.
+    az_downscale = round(numpy.mean(ad[ad > 0])) > 0.7
+    # As long as it is not legacy, we expand it to 720
+    # Then we fill the shape
+    if az_downscale:
+        for k in range(r.shape[0]):
+            az_index = int(a[k] * 2)
+            dump_array[elevation_index, az_index % size_y, :] = r[k, :size_z]
+            dump_array[elevation_index, (az_index + 1) % size_y, :] = r[k, :size_z]
+    else:
+        for k in range(r.shape[0]):
+            az_index = int(a[k] * 2)
+            dump_array[elevation_index, az_index % size_y, :] = r[k, :size_z]
+
+def fill_data_function(gate_array, azimuth_array, dump_array, elevation_index, size_y, size_z):
+    try:
+        fill_data_jit(gate_array, azimuth_array, dump_array, elevation_index, size_y, size_z)
+    except:
+        logging.warning("Failed to run JIT functions")
+        fill_data_nojit(gate_array, azimuth_array, dump_array, elevation_index, size_y, size_z)
+
 
 class RadarNetcdf(object):
     VcpTable = {
@@ -46,42 +104,54 @@ class RadarNetcdf(object):
     ref_var_names = ["Reflectivity", "DifferentialReflectivity", "CorrelationCoefficient", "DifferentialPhase"]
     vel_var_names = ["RadialVelocity", "SpectrumWidth"]
 
-    all_var_suffix = list("RVSDCP")
+    all_var_suffix = list("RVVDCP")
     ref_var_suffix = list("RDCP")
-    vel_var_suffix = list("VS")
-
+    vel_var_suffix = list("VV")
 
     def __init__(self, dataset: Dataset, legacy: bool = False, dualpol: bool = True):
 
         self.dataset = dataset
-        self.vcp_mode = self.VcpTable[self.dataset.VolumeCoveragePattern]
-
-        logging.info(self.vcp_mode)
-        self.array_shape = (len(self.vcp_mode), 360, 230) if legacy else (len(self.vcp_mode), 720, 920)
         self.legacy_mode = legacy
         self.dualpol = dualpol
 
+        self.vcp_mode = self.VcpTable[self.dataset.VolumeCoveragePattern]
 
+        logging.info(self.vcp_mode)
+
+        self.beams_count = 360 if self.legacy_mode else 720
+        self.gates_count = 230 if self.legacy_mode else 920
+        self.array_shape = (len(self.vcp_mode), self.beams_count, self.gates_count)
 
         # Copy entire data from netCDF file to memory
         self.data_dict = {
-            "Reflectivity": numpy.zeros(self.array_shape, dtype='f4'),
-            "RadialVelocity": numpy.zeros(self.array_shape, dtype='f4'),
-            "SpectrumWidth": numpy.zeros(self.array_shape, dtype='f4'),
-            "DifferentialReflectivity": numpy.zeros(self.array_shape, dtype='f4'),
-            "CorrelationCoefficient": numpy.zeros(self.array_shape, dtype='f4'),
-            "DifferentialPhase": numpy.zeros(self.array_shape, dtype='f4')
+            "Reflectivity": numpy.empty(self.array_shape, dtype='f4'),
+            "RadialVelocity": numpy.empty(self.array_shape, dtype='f4'),
+            "SpectrumWidth": numpy.empty(self.array_shape, dtype='f4'),
+            "DifferentialReflectivity": numpy.empty(self.array_shape, dtype='f4'),
+            "CorrelationCoefficient": numpy.empty(self.array_shape, dtype='f4'),
+            "DifferentialPhase": numpy.empty(self.array_shape, dtype='f4')
         }  # type: Dict[str, numpy.ndarray]
+
+        for p in self.data_dict.values():
+            p.fill(-99990.0)
+
+        # Filling status, ensure everything is filled!
+        self.filling_table = numpy.zeros((len(self.vcp_mode), len(self.data_dict)), dtype='b1')
 
         self.time_data_dict = OrderedDict()  # type: Dict[int, Dict[str, numpy.ma.MaskedArray]
         self.dump_data_by_time(self.all_var_names, self.time_data_dict)
         self.dump_data_by_time(["elevation" + p for p in self.all_var_suffix], self.time_data_dict)
+        self.dump_data_by_time(["azimuth" + p for p in self.all_var_suffix], self.time_data_dict)
 
-        self.fill_data()
+        self.fill_data(first_pass=True)
+
+    def is_filled(self):
+        # All filled means all is True
+        return numpy.all(numpy.logical_not(self.filling_table))  # type: bool
 
     # Dump helper function
     def __dump(self, var, time_var, dump_dict):
-        if not time_var in ds.variables or not var in ds.variables:
+        if time_var not in ds.variables or var not in ds.variables:
             return
         time_var_dump = ds.variables[time_var][:]
         var_dump = self.__fix_variable__(self.dataset.variables[var])
@@ -122,20 +192,47 @@ class RadarNetcdf(object):
     def first_pass(self):
         pass
 
+    def fill_data(self, *args, **kwargs):
 
-
-    def fill_data(self):
         for i in self.time_data_dict.values():
             # There are several cases:
             # Case 1: CS case, only reflectivity related variable will be here
-            if not 'elevationV' in i and not 'elevationV_HI' in i:
+            if 'elevationV' not in i and 'elevationV_HI' not in i:
                 mode = "_HI" if 'elevationR_HI' in i else ""
                 elevation = i["elevationR" + mode].mean()
                 elevation_index = find_elevation_index(self.vcp_mode, elevation)
-                logging.debug("%f - %d" % (elevation, elevation_index))
-                # Case 2: Normal case, all variables will be here.
-
-                #
+                logging.info("%f - %d" % (elevation, elevation_index))
+                for j, s in zip(self.ref_var_names, self.ref_var_suffix):
+                    logging.debug("Dumping variable: %s - %s" % (j, s))
+                    fill_data_function(i[j + mode], i["azimuth" + s + mode], self.data_dict[j], elevation_index,
+                                    self.beams_count, self.gates_count)
+                    pass
+            # CD pulses: we only use Doppler Velocity
+            elif (('elevationV' in i or 'elevationV_HI' in i) and ('elevationR' not in i and 'elevationR_HI' not in i)) \
+                    or (('elevationV' in i or 'elevationV_HI' in i) and
+                            ('elevationR' in i or 'elevationR_HI' in i) and
+                            ('DifferentialReflectivity' not in i and 'DifferentialReflectivity' not in i)
+                        ):
+                mode = "_HI" if 'elevationV_HI' in i else ""
+                elevation = i["elevationV" + mode].mean()
+                elevation_index = find_elevation_index(self.vcp_mode, elevation)
+                logging.info("%f - %d" % (elevation, elevation_index))
+                for j, s in zip(self.vel_var_names, self.vel_var_suffix):
+                    logging.debug("Dumping variable: %s - %s" % (j, s))
+                    fill_data_function(i[j + mode], i["azimuth" + s + mode], self.data_dict[j], elevation_index,
+                                    self.beams_count, self.gates_count)
+                    pass
+            # Normal case, all variables will be gathered in one scan
+            else:
+                mode = "_HI" if 'elevationR_HI' in i else ""
+                elevation = i["elevationR" + mode].mean()
+                elevation_index = find_elevation_index(self.vcp_mode, elevation)
+                logging.info("%f - %d" % (elevation, elevation_index))
+                for j, s in zip(self.all_var_names, self.all_var_suffix):
+                    logging.debug("Dumping variable: %s - %s" % (j, s))
+                    fill_data_function(i[j + mode], i["azimuth" + s + mode], self.data_dict[j], elevation_index,
+                                    self.beams_count, self.gates_count)
+                    pass
 
         pass
 
@@ -164,5 +261,5 @@ class RadarNetcdf(object):
 
 
 if __name__ == "__main__":
-    ds = Dataset('/home/sugar/Developer/Cloud2016/KDOX20121029_002042_V07.nc', 'r')
+    ds = Dataset('/home/sugar/Developer/Cloud2016/KTLH20160902_000332_V06.nc', 'r')
     R = RadarNetcdf(ds)
