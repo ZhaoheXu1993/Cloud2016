e89a0fedc3c14de86b6c473d31095710d0f6679e
sugar <sugar1987cn@gmail.com>
2016-11-14 19:21:52 -0500

    restart new alg

--- data_handler_v2.py
+++ data_handler_v2.py
@@ -2,9 +2,13 @@
 import time
 from datetime import datetime, timedelta
 
+from numba import jit
 from sortedcontainers import SortedDict as OrderedDict
 
 from functools import reduce
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 import json
 
@@ -19,6 +23,12 @@ from netCDF4 import Dataset, num2date
 from typing import List, Dict, Tuple
 
 
+@jit
+def find_elevation_index(vcp_mode, e):
+    for i in vcp_mode:
+        if -0.2 < e - i < 0.2:
+            return i
+
 class RadarNetcdf(object):
     VcpTable = {
         11: (0.5, 1.5, 2.4, 3.4, 4.3, 5.3, 6.2, 7.5, 8.7, 10.0, 12.0, 14.0, 16.7, 19.5),
@@ -30,23 +40,28 @@ class RadarNetcdf(object):
         221: (0.5, 1.5, 2.4, 3.4, 4.3, 6.0, 9.9, 14.6, 19.5)
     }
 
-    var_names = ["Reflectivity", "RadialVelocity", "SpectrumWidth",
-                 "DifferentialReflectivity", "CorrelationCoefficient", "DifferentialPhase"]
-    var_suffix = list("RVSDCP")
+    all_var_names = ["Reflectivity",
+                     "RadialVelocity", "SpectrumWidth",
+                     "DifferentialReflectivity", "CorrelationCoefficient", "DifferentialPhase"]
+    ref_var_names = ["Reflectivity", "DifferentialReflectivity", "CorrelationCoefficient", "DifferentialPhase"]
+    vel_var_names = ["RadialVelocity", "SpectrumWidth"]
 
-    velocity_var_list = {"RadialVelocity", "SpectrumWidth", "RadialVelocity_HI", "SpectrumWidth_HI"}
+    all_var_suffix = list("RVSDCP")
+    ref_var_suffix = list("RDCP")
+    vel_var_suffix = list("VS")
 
-    ScanTable = (0.5, 0.5, 0.9, 0.9, 1.3, 1.3, 1.8, 2.4, 3.1, 4.0, 5.1, 6.4, 8.0, 10.0, 12.5, 15.6, 19.5)
-    ScanVariable = (1, 2, 1, 2, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
 
-    def __init__(self, dataset: Dataset, legacy: bool = False):
-        """
-        :dataset: an opened object of `netCDF4.Dataset`
-        """
+    def __init__(self, dataset: Dataset, legacy: bool = False, dualpol: bool = True):
+
         self.dataset = dataset
         self.vcp_mode = self.VcpTable[self.dataset.VolumeCoveragePattern]
+
+        logging.info(self.vcp_mode)
         self.array_shape = (len(self.vcp_mode), 360, 230) if legacy else (len(self.vcp_mode), 720, 920)
         self.legacy_mode = legacy
+        self.dualpol = dualpol
+
+
 
         # Copy entire data from netCDF file to memory
         self.data_dict = {
@@ -58,79 +73,38 @@ class RadarNetcdf(object):
             "DifferentialPhase": numpy.zeros(self.array_shape, dtype='f4')
         }  # type: Dict[str, numpy.ndarray]
 
-        self.time_data_dict = OrderedDict()
-        self.dump_data_by_time(self.var_names, self.time_data_dict)
-        self.dump_data_by_time(["elevation" + p for p in self.var_suffix], self.time_data_dict)
+        self.time_data_dict = OrderedDict()  # type: Dict[int, Dict[str, numpy.ma.MaskedArray]
+        self.dump_data_by_time(self.all_var_names, self.time_data_dict)
+        self.dump_data_by_time(["elevation" + p for p in self.all_var_suffix], self.time_data_dict)
+
+        self.fill_data()
+
+    # Dump helper function
+    def __dump(self, var, time_var, dump_dict):
+        if not time_var in ds.variables or not var in ds.variables:
+            return
+        time_var_dump = ds.variables[time_var][:]
+        var_dump = self.__fix_variable__(self.dataset.variables[var])
+        assert time_var_dump.min(axis=1).data == time_var_dump[:, 0].data
+        scan_start_time = time_var_dump.min(axis=1)
+        for t in range(scan_start_time.shape[0]):
+            logging.debug("%s - %d - %d" % (var, t, scan_start_time[t]))
+            try:
+                dump_dict[scan_start_time[t]][var] = var_dump[t]
+            except KeyError:
+                dump_dict[scan_start_time[t]] = OrderedDict({var: var_dump[t]})
 
     def dump_data_by_time(self, dump_var_names, dump_target):
-
-        # Dump helper function
-        def __dump(var, time_var, dump_dict):
-            if not time_var in ds.variables or not var in ds.variables:
-                return
-            time_var_dump = ds.variables[time_var][:]
-            var_dump = ds.variables[var][:]
-            assert time_var_dump.min(axis=1).data == time_var_dump[:, 0].data
-            scan_start_time = time_var_dump.min(axis=1)
-            for t in range(scan_start_time.shape[0]):
-                print(var, t, scan_start_time[t])
-                try:
-                    dump_dict[scan_start_time[t]][var] = var_dump[t]
-                except KeyError:
-                    dump_dict[scan_start_time[t]] = OrderedDict({var: var_dump[t]})
-
-        assert len(dump_var_names) == len(self.var_suffix)
-
+        assert len(dump_var_names) == len(self.all_var_suffix)
         for i in range(len(dump_var_names)):
             # Dump general variables
             var = dump_var_names[i]
-            time_var = "time" + self.var_suffix[i]
-            __dump(var, time_var, dump_target)
+            time_var = "time" + self.all_var_suffix[i]
+            self.__dump(var, time_var, dump_target)
             # Dump high_resolution variables
             var_hi = var + "_HI"
             time_var_hi = time_var + "_HI"
-            __dump(var_hi, time_var_hi, dump_target)
-
-    def order_data(self):
-        """Restore the order of data, by scanning start time."""
-        # Scan start time at each elevation.
-        ds = self.dataset
-        start_timeR = ds.variables['timeR'][:].min(axis=1)
-        # If there is high resolution data, merge it
-        start_timeR_HI = ds.variables['timeR_HI'][:].min(axis=1) if "Reflectivity_HI" in ds.variables else []
-        timeR_sort = numpy.concatenate((start_timeR, start_timeR_HI)).argsort()
-        self.ref_sort_order = timeR_sort
-        self.sorted_timeR = numpy.concatenate((start_timeR, start_timeR_HI)).sort()
-
-        # ElevationR
-        elevationR = ds.variables['elevationR'][:].mean(axis=1)
-        elevationR_HI = ds.variables['elevationR_HI'][:].mean(axis=1) if "Reflectivity_HI" in ds.variables else []
-        all_elevationR = numpy.concatenate((elevationR, elevationR_HI))
-        sorted_elevationR = all_elevationR[timeR_sort]
-
-        # Once we get sorted elevation, we should extract SAILS elevations.
-        # Problem is, how can we deal with these elevations
-        self.normalized_elevation = sorted_elevationR.round(1)  # type: numpy.ma.MaskedArray
-
-        # Then we check if elevationV is all included in elevationR.
-        # If true, means CD contains reflectivity data, we need drop them, if false, we merge velocity data
-        start_timeV = ds.variables['timeV'][:].min(axis=1)
-        start_timeV_HI = ds.variables['timeV_HI'][:].min(axis=1)
-        timeV_sort = numpy.concatenate((start_timeV, start_timeV_HI)).argsort()
-        self.vel_sort_order = timeV_sort
-        self.sorted_timeV = numpy.concatenate((start_timeV, start_timeV_HI)).sort()
-
-    def tag_elevation(self):
-        # First, all SAILS elevation is at 0.5, we can assume, it is definitely less than 0.7
-        self.is_sails = numpy.zeros_like(self.normalized_elevation)
-        j = 0
-        for i in range(0, self.normalized_elevation.shape[0]):
-            # Just in case, rounded value is wrong, maximum error would be ~0.1
-            if abs(self.normalized_elevation[i] - self.ScanTable[j]) < 0.2:
-                self.is_sails[i] = 0
-                j += 1
-            else:
-                self.is_sails[i] = 1
+            self.__dump(var_hi, time_var_hi, dump_target)
 
     @staticmethod
     def __fix_variable__(v: netCDF4.Variable) -> numpy.ma.MaskedArray:
@@ -140,14 +114,35 @@ class RadarNetcdf(object):
             # Dump out
             _v = v[:]
             _v1 = _v.data.view('u' + str(v.datatype)) * v.scale_factor + v.add_offset
+            logging.info("Fix unsigned variable:" + v.name)
             return numpy.ma.MaskedArray(_v1, _v.mask)
         else:
             return v[:]
 
+    def first_pass(self):
+        pass
+
+
+
+    def fill_data(self):
+        for i in self.time_data_dict.values():
+            # There are several cases:
+            # Case 1: CS case, only reflectivity related variable will be here
+            if not 'elevationV' in i and not 'elevationV_HI' in i:
+                mode = "_HI" if 'elevationR_HI' in i else ""
+                elevation = i["elevationR" + mode].mean()
+                elevation_index = find_elevation_index(self.vcp_mode, elevation)
+                logging.debug("%f - %d" % (elevation, elevation_index))
+                # Case 2: Normal case, all variables will be here.
+
+                #
+
+        pass
+
     def mask_data(self):
         # Create a mask to remove all invalid data from reflectivity scan.
         total_mask = reduce(numpy.ma.logical_or,
-                            tuple([v.mask for k, v in self.data_dict if k not in velocity_var_list]))
+                            tuple([v.mask for k, v in self.data_dict]))
         # Update mask
         for v in self.data_dict.values():
             v.mask = total_mask
